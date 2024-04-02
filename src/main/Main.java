a6d1030afff416d19b5cce871829fac0a0477d47
cristianbtzk <cristian.btzk@gmail.com>
2024-04-02 10:05:22 -0300

    dynamic filenames

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,32 +7,17 @@ import java.util.concurrent.Executors;
 public class Main {
 	public static void main(String[] args) {
 		
+		String[] filenames = {"dna-0", "dna-1", "dna-2", "dna-3", "dna-4", "dna-5", "dna-6", "dna-7", "dna-8", "dna-9"};
+		
 		
 		ReplicacaoDNA rd= new ReplicacaoDNA();
-		ThreadCadeiaComplementar t1 = new ThreadCadeiaComplementar("dna-0", rd);
-		ThreadCadeiaComplementar t2 = new ThreadCadeiaComplementar("dna-1", rd);
-		ThreadCadeiaComplementar t3 = new ThreadCadeiaComplementar("dna-2", rd);
-		ThreadCadeiaComplementar t4 = new ThreadCadeiaComplementar("dna-3", rd);
-		ThreadCadeiaComplementar t5 = new ThreadCadeiaComplementar("dna-4", rd);
-		ThreadCadeiaComplementar t6 = new ThreadCadeiaComplementar("dna-5", rd);
-		ThreadCadeiaComplementar t7 = new ThreadCadeiaComplementar("dna-6", rd);
-		ThreadCadeiaComplementar t8 = new ThreadCadeiaComplementar("dna-7", rd);
-		ThreadCadeiaComplementar t9 = new ThreadCadeiaComplementar("dna-8", rd);
-		ThreadCadeiaComplementar t10 = new ThreadCadeiaComplementar("dna-9", rd);
 		
-		System.out.println("Início - executor");
 		ExecutorService executorService = Executors.newCachedThreadPool();
-		executorService.execute(t1);
-		executorService.execute(t2);
-		executorService.execute(t3);
-		executorService.execute(t4);
-		executorService.execute(t5);
-		executorService.execute(t6);
-		executorService.execute(t7);
-		executorService.execute(t8);
-		executorService.execute(t9);
-		executorService.execute(t10);
-		executorService.shutdown();
-		System.out.println("Fim - executor");;
+
+		for (String file: filenames) {
+			ThreadCadeiaComplementar t = new ThreadCadeiaComplementar(file, rd);
+			executorService.execute(t);
+		}
+		
 	}
 }
